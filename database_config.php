14c8106b93b80bdd63e3f4db7d98f9c833cb5a0d
gorkemer <gorkemerr@gmail.com>
2020-06-21 09:54:53 +0300

    add database

--- database_config.php
+++ database_config.php
@@ -3,6 +3,6 @@
   $port = 3306;
   $username = "root";
   $password = "karabiber10"; //if it asks mysql user root, if not: nyhe86bp
-  $dbname = "EmoReguDatabase";
+  $dbname = "gainLossDatabase";
   $table = "dataColumns";
 ?>
